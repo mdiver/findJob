451db4ad29555fa48e1efffdcc9e6c14418814e3
limjongsu <imjongsu@gmail.com>
2019-08-28 21:12:15 +0900

    Delete findjob

--- findjob/company/src/main/java/com/sk/sample/mall/account/domain/model/MemberType.java
+++
@@ -1,6 +0,0 @@
-package com.sk.sample.mall.account.domain.model;
-
-public enum MemberType {
-	SELLER,
-	BUYER
-}
--- findjob/company/src/main/java/com/sk/sample/mall/account/domain/model/MembershipLevelType.java
+++
@@ -1,7 +0,0 @@
-package com.sk.sample.mall.account.domain.model;
-
-public enum MembershipLevelType {
-	VIP,
-	GOLD,
-	SILVER
-}
